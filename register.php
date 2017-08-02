c1d990bfc00e3a92b6c6e823312d5be1f49eb0a2
ECOMSOLVER <WPSHOPLA@users.noreply.github.com>
2017-08-02 16:11:12 +0800

    2 columns for register page

--- register.php
+++ register.php
@@ -142,140 +142,141 @@ include('templates/default/header.php');
 ?>
     <div class="container-fluid content form-only-content">
         <div class="main-container">
-            <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4">
-                <div class="signup-form">
+
+            <?php
+            if ($invited && !$invite_info) {
+                echo '<div class="search-examples"><center><p><b>Invalid invite link</b></p></center></div>';
+            } else {
+                if ($invited && $invite_info) {
+                    $ownerDetails = $userClass->userDetails($invite_info->user_id);
+                    echo '<div class="search-examples"><center><p><b>Join ' . $ownerDetails->first_name . ' ' . $ownerDetails->last_name . '\'s team</b></p></center></div>';
+                }
+            }
+            ?>
+
+            <div class="row signup-form">
+                <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-2 col-lg-4 col-lg-offset-2">
                     <?php echo $notifyMsgRegister; ?>
-
-                    <?php
-                    if ($invited && !$invite_info) {
-                        echo '<div class="search-examples"><center><p><b>Invalid invite link</b></p></center></div>';
-                    } else {
-                        if ($invited && $invite_info) {
-                            $ownerDetails = $userClass->userDetails($invite_info->user_id);
-                            echo '<div class="search-examples"><center><p><b>Join ' . $ownerDetails->first_name . ' ' . $ownerDetails->last_name . '\'s team</b></p></center></div>';
-                        }
-                        ?>
-
-                        <div class="h1 text-blue">Sign Up</div>
-
-                        <form name="form" method="post">
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-6">
-                                    <div class="form-group<?php if ($errorMsgFirstName != '') echo ' has-error'; ?>">
-                                        <?php if ($errorMsgFirstName != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgFirstName . '</li></ul></span>'; ?>
-                                        <input type="text" name="first_name"
-                                               value="<?php echo htmlspecialchars($first_name, ENT_QUOTES, 'UTF-8'); ?>"
-                                               class="form-control" placeholder="First Name" required>
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-sm-6">
-                                    <div class="form-group<?php if ($errorMsgLastName != '') echo ' has-error'; ?>">
-                                        <?php if ($errorMsgLastName != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgLastName . '</li></ul></span>'; ?>
-                                        <input type="text" name="last_name"
-                                               value="<?php echo htmlspecialchars($last_name, ENT_QUOTES, 'UTF-8'); ?>"
-                                               class="form-control" placeholder="Last Name" required>
-                                    </div>
+                    <div class="h1 text-blue">Sign Up</div>
+                    <p class="text-info">
+                        GoFetchCode provides answers to your Building Code-related questions in a snap.
+                        <br><br>As soon as you fill in the form on this page and subscribe, your free trial account
+                        will
+                        start.
+                        After 7 days, simply subscribe to one of our paid plans and keep using GoFetchCode for
+                        your building code needs.
+                        <br><br>Register now and get immediate and free access to the GoFetchCode search engine.
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <form name="form" method="post">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group<?php if ($errorMsgFirstName != '') echo ' has-error'; ?>">
+                                    <?php if ($errorMsgFirstName != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgFirstName . '</li></ul></span>'; ?>
+                                    <input type="text" name="first_name"
+                                           value="<?php echo htmlspecialchars($first_name, ENT_QUOTES, 'UTF-8'); ?>"
+                                           class="form-control" placeholder="First Name" required>
                                 </div>
                             </div>
-
-                            <div class="form-group<?php if ($errorMsgUsername != '') echo ' has-error'; ?>">
-                                <?php if ($errorMsgUsername != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgUsername . '</li></ul></span>'; ?>
-                                <input type="text" name="username"
-                                       value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?>"
-                                       class="form-control" placeholder="Username" required>
-                            </div>
-
-                            <div class="form-group<?php if ($errorMsgPassword != '') echo ' has-error'; ?>">
-                                <?php if ($errorMsgPassword != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgPassword . '</li></ul></span>'; ?>
-                                <input type="password" name="password"
-                                       value="<?php echo htmlspecialchars($password, ENT_QUOTES, 'UTF-8'); ?>"
-                                       class="form-control" placeholder="Password" required>
-                            </div>
-
-                            <div class="form-group<?php if ($errorMsgPassword2 != '') echo ' has-error'; ?>">
-                                <?php if ($errorMsgPassword2 != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgPassword2 . '</li></ul></span>'; ?>
-                                <input type="password" name="password_2"
-                                       value="<?php echo htmlspecialchars($password_2, ENT_QUOTES, 'UTF-8'); ?>"
-                                       class="form-control" placeholder="Confirm Password" required>
-                            </div>
-
-                            <div class="form-group<?php if ($errorMsgEmail != '') echo ' has-error'; ?>">
-                                <?php if ($errorMsgEmail != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgEmail . '</li></ul></span>'; ?>
-                                <input type="email" name="email"
-                                       value="<?php echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8'); ?>"
-                                       class="form-control" placeholder="Email" required>
-                            </div>
-
-                            <div class="form-group<?php if ($errorMsgPhone != '') echo ' has-error'; ?>">
-                                <?php if ($errorMsgPhone != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgPhone . '</li></ul></span>'; ?>
-                                <input type="text" name="phone"
-                                       value="<?php echo htmlspecialchars($phone, ENT_QUOTES, 'UTF-8'); ?>"
-                                       class="form-control" placeholder="Phone" required>
-                            </div>
-
-                            <div class="form-group<?php if ($errorMsgOrg != '') echo ' has-error'; ?>">
-                                <?php if ($errorMsgOrg != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgOrg . '</li></ul></span>'; ?>
-                                <input type="text" name="organization"
-                                       value="<?php echo htmlspecialchars($organization, ENT_QUOTES, 'UTF-8'); ?>"
-                                       class="form-control" placeholder="Organization" required>
-                            </div>
-
-                            <?php if (false && !$invited) { ?>
-                                <div class="form-group<?php if ($errorMsgExtraUsers != '') echo ' has-error'; ?>">
-                                    <label>Amount of sub-accounts(for teams)</label>
-                                    <?php if ($errorMsgExtraUsers != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgExtraUsers . '</li></ul></span>'; ?>
-                                    <select name="extra_users" class="form-control">
-                                        <?php
-                                        foreach ($extra_users_tiers as $tier) {
-                                            $selected = $extra_users == $tier ? ' selected' : '';
-                                            echo '<option value="' . $tier . '"' . $selected . '>' . $tier . '</option>';
-                                        }
-                                        ?>
-                                    </select>
-
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group<?php if ($errorMsgLastName != '') echo ' has-error'; ?>">
+                                    <?php if ($errorMsgLastName != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgLastName . '</li></ul></span>'; ?>
+                                    <input type="text" name="last_name"
+                                           value="<?php echo htmlspecialchars($last_name, ENT_QUOTES, 'UTF-8'); ?>"
+                                           class="form-control" placeholder="Last Name" required>
                                 </div>
+                            </div>
+                        </div>
 
-                                <div class="form-group<?php if ($errorMsgLocation != '') echo ' has-error'; ?>">
-                                    <label>Locations</label>
-                                    <?php if ($errorMsgLocation != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgLocation . '</li></ul></span>'; ?>
-                                    <div class="form-control">
-                                        <?php
-                                        foreach ($mainClass->getStates() as $state) {
-                                            if ($state['id'] != 5 && $state['id'] != 10)
-                                                continue;
+                        <div class="form-group<?php if ($errorMsgUsername != '') echo ' has-error'; ?>">
+                            <?php if ($errorMsgUsername != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgUsername . '</li></ul></span>'; ?>
+                            <input type="text" name="username"
+                                   value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?>"
+                                   class="form-control" placeholder="Username" required>
+                        </div>
 
-                                            echo '<label class="col-sm-6 col-md-6"><input type="checkbox" name="locations[]" value="' . $state['id'] . '">' . $state['name'] . '</label>';
-                                        }
-                                        ?>
-                                    </div>
+                        <div class="form-group<?php if ($errorMsgPassword != '') echo ' has-error'; ?>">
+                            <?php if ($errorMsgPassword != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgPassword . '</li></ul></span>'; ?>
+                            <input type="password" name="password"
+                                   value="<?php echo htmlspecialchars($password, ENT_QUOTES, 'UTF-8'); ?>"
+                                   class="form-control" placeholder="Password" required>
+                        </div>
+
+                        <div class="form-group<?php if ($errorMsgPassword2 != '') echo ' has-error'; ?>">
+                            <?php if ($errorMsgPassword2 != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgPassword2 . '</li></ul></span>'; ?>
+                            <input type="password" name="password_2"
+                                   value="<?php echo htmlspecialchars($password_2, ENT_QUOTES, 'UTF-8'); ?>"
+                                   class="form-control" placeholder="Confirm Password" required>
+                        </div>
+
+                        <div class="form-group<?php if ($errorMsgEmail != '') echo ' has-error'; ?>">
+                            <?php if ($errorMsgEmail != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgEmail . '</li></ul></span>'; ?>
+                            <input type="email" name="email"
+                                   value="<?php echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8'); ?>"
+                                   class="form-control" placeholder="Email" required>
+                        </div>
+
+                        <div class="form-group<?php if ($errorMsgPhone != '') echo ' has-error'; ?>">
+                            <?php if ($errorMsgPhone != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgPhone . '</li></ul></span>'; ?>
+                            <input type="text" name="phone"
+                                   value="<?php echo htmlspecialchars($phone, ENT_QUOTES, 'UTF-8'); ?>"
+                                   class="form-control" placeholder="Phone" required>
+                        </div>
+
+                        <div class="form-group<?php if ($errorMsgOrg != '') echo ' has-error'; ?>">
+                            <?php if ($errorMsgOrg != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgOrg . '</li></ul></span>'; ?>
+                            <input type="text" name="organization"
+                                   value="<?php echo htmlspecialchars($organization, ENT_QUOTES, 'UTF-8'); ?>"
+                                   class="form-control" placeholder="Organization" required>
+                        </div>
+
+                        <?php if (false && !$invited) { ?>
+                            <div class="form-group<?php if ($errorMsgExtraUsers != '') echo ' has-error'; ?>">
+                                <label>Amount of sub-accounts(for teams)</label>
+                                <?php if ($errorMsgExtraUsers != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgExtraUsers . '</li></ul></span>'; ?>
+                                <select name="extra_users" class="form-control">
+                                    <?php
+                                    foreach ($extra_users_tiers as $tier) {
+                                        $selected = $extra_users == $tier ? ' selected' : '';
+                                        echo '<option value="' . $tier . '"' . $selected . '>' . $tier . '</option>';
+                                    }
+                                    ?>
+                                </select>
+
+                            </div>
+
+                            <div class="form-group<?php if ($errorMsgLocation != '') echo ' has-error'; ?>">
+                                <label>Locations</label>
+                                <?php if ($errorMsgLocation != '') echo '<span class="help-block with-errors"><ul class="list-unstyled"><li>' . $errorMsgLocation . '</li></ul></span>'; ?>
+                                <div class="form-control">
+                                    <?php
+                                    foreach ($mainClass->getStates() as $state) {
+                                        if ($state['id'] != 5 && $state['id'] != 10)
+                                            continue;
+
+                                        echo '<label class="col-sm-6 col-md-6"><input type="checkbox" name="locations[]" value="' . $state['id'] . '">' . $state['name'] . '</label>';
+                                    }
+                                    ?>
                                 </div>
-
-                            <?php } ?>
-
-                            <div class="form-group">
-                                <label><input type="checkbox" id="agree">&emsp; I agree to the <a href="termsofservice.php"
-                                                                                              target="_blank">GofetchCode
-                                        Terms and Conditions</a></label>
                             </div>
 
-                            <div class="form-actions form-group ">
-                                <input type="submit" name="register_submit" class="pri_button full-width" disabled
-                                       id="signup_submit"
-                                       value="Sign up for a free trial">
-                            </div>
+                        <?php } ?>
 
-                        </form>
+                        <div class="form-group">
+                            <label><input type="checkbox" id="agree">&emsp; I agree to the <a
+                                    href="termsofservice.php"
+                                    target="_blank">GofetchCode
+                                    Terms and Conditions</a></label>
+                        </div>
 
-                        <p class="text-info">
-                            GoFetchCode provides answers to your Building Code-related questions in a snap.
-                            <br><br>As soon as you fill in the form on this page and subscribe, your free trial account will
-                            start.
-                            After 7 days, simply subscribe to one of our paid plans and keep using GoFetchCode for
-                            your building code needs.
-                            <br><br>Register now and get immediate and free access to the GoFetchCode search engine.
-                        </p>
-                    <?php } ?>
+                        <div class="form-actions form-group ">
+                            <input type="submit" name="register_submit" class="pri_button full-width" disabled
+                                   id="signup_submit"
+                                   value="Sign up for a free trial">
+                        </div>
 
+                    </form>
                 </div>
             </div>
         </div>
